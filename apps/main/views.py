c43d2e7f4f223c642ec8956c8d13d12162c9b8cc
JosephAMumford <JosephAMumford@gmail.com>
2018-04-03 18:54:20 -0500

    update

--- apps/main/views.py
+++ apps/main/views.py
@@ -19,17 +19,14 @@ def index(request):
         options_paragraphs = 5
         quotes = []
 
-        try:
-            if 'character_ids' in request.session:
-                character_ids = request.session['character_ids']
+        if 'character_ids' in request.session:
+            character_ids = request.session['character_ids']
             
-            else:
-                character_ids = ['1','2','3','4','5','6','7','8','9','10',
-                                '11','12','13','14','15','16','17','18','19','20',
-                                '21','22','23','24','25','26','27','28','29','30',
-                                '31','32','33','34','35','36']
-        except:
-            error += ":Character ID error"
+        else:
+            character_ids = ['1','2','3','4','5','6','7','8','9','10',
+                            '11','12','13','14','15','16','17','18','19','20',
+                            '21','22','23','24','25','26','27','28','29','30',
+                            '31','32','33','34','35','36']
         
         if 'paragraphs' in request.session:
             options_paragraphs = int(request.session['paragraphs']) * 10
